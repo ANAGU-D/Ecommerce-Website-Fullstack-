56104318465a763d329d2dadcd4a41047ad239df
ANAGU DANIEL <anagudaniel@gmail.com>
2023-01-06 03:24:25 +0100

    'ft:added'

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,20 +6,47 @@ const Container = styled.div`
         width:100%;
         height:100vh;
         display: flex;
+        position:relative;
 `
 const Arrow = styled.div`
 width:50px;
 height: 50px;
-backgrond-color:#fff7f7;
-border-radius:50%
+background-color:#fff7f7;
+border-radius:50%;
+display:flex;
+align-items:center;
+justify-content: center;
+position:absolute;
+top:0;
+bottom:0;
+left:${props=> props.direction==='left'&& '10px'};
+right:${props=> props.direction==='right'&& '10px'};
+margin:auto;
+cursor:pointer;
+opacity:0.5;
+`
+const Wrapper =styled.div`
+   height:100%
+` 
+const Slide =styled.div`
+ display:flex;
+ align-items:center;
+`
+const ImgContainer =styled.div`
+flex:1;
+`
+
+const InfoContainer =styled.div`
+  flex:1;
 `
 function Slider() {
   return (
    <Container>
-        <Arrow>
+        <Arrow direction='left'>
             <ArrowLeftOutlined/>
+        </Arrow>
+        <Arrow direction='right'>
             <ArrowRightOutlined/>
-
         </Arrow>
    </Container>
   )
