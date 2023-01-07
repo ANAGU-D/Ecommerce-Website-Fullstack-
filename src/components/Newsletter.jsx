f7586881e60e1d1e32ab08c8f04bc33f0f907599
ANAGU DANIEL <anagudaniel@gmail.com>
2023-01-07 17:33:09 +0100

    feat

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,22 +1,53 @@
+import { Send } from '@mui/icons-material'
 import React from 'react'
 import styled from 'styled-components'
 
 
-const Container= styled.div``
-const Title= styled.h1``
-const Description= styled.p``
-const InputContainer= styled.div``
-const Input= styled.input``
-const Button= styled.button``
+const Container= styled.div`
+ height:60vh;
+ background-color:#fcf5f5;
+ display:flex;
+ align-items:center;
+ justify-content:center;
+ flex-direction:column;
+`
+const Title= styled.h1`
+font-sie:70px;
+margin-bottom:20px;
+`
+const Description= styled.p`
+font-size:24px;
+font-weight:300;
+margin-bottom:20px;
+`
+const InputContainer= styled.div`
+width:50%;
+height:40px;
+background-color:white;
+display:flex;
+justify-content:space-between;
+border:1px solid lightgray;
+`
+const Input= styled.input`
+border:none;
+flex:8;
+padding-left:20px
+`
+const Button= styled.button`
+flex:1;
+border:none;
+background-color:teal;
+color:white;
+`
 function Newsletter() {
   return (
     <Container>
         <Title>NEWSLETTER</Title>
-        <Description>Signup for our newsletter</Description>
+        <Description>Get Timely Update from Your Favourite Products</Description>
         <InputContainer>
-            <Input/>
+            <Input placeholder=' Your E-mail Address'/>
              <Button>
-                SIGNUP
+               <Send/>
              </Button>
         </InputContainer>
     </Container>
